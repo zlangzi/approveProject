e4274f5ef6c04e517f3b907a2cfda6cbcb63f49f
noidler <1148402529@qq.com>
2016-09-26 23:54:56 +0800

    搞错...

--- WebRoot/WebRoot/js/loadFile.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-    	$('nav').load("http://localhost:8080/approveProject/html/nav.html");
-        $('#reglog').load("http://localhost:8080/approveProject/html/register.html");
-        $('footer').load("http://localhost:8080/approveProject/html/footer.html")
-    })
