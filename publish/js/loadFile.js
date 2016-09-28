147241dc3a08a3770f66685d7446b450ecccae64
noidler <1148402529@qq.com>
2016-09-28 13:10:12 +0800

    准备写测试文档的一次发布

---
+++ publish/js/loadFile.js
@@ -0,0 +1,5 @@
+$(function(){
+    	$('nav').load("/approveProject/html/nav.html");
+        $('#reglog').load("/approveProject/html/register.html");
+        $('footer').load("/approveProject/html/footer.html")
+    })
